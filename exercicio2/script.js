9711cbd30caefbb251e816e0dc661c98555fc7c4
falacomomenna <danielmenna@gmail.com>
2022-07-31 19:34:19 -0300

    resolução do exercicio 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,5 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = 'Eu não sou supersticioso, mas sou um pouco ________.      '
+
+console.log(minhaString.length)
+console.log(minhaString.trim().length)
+console.log(minhaString.replace('________', 'sticioso'))
